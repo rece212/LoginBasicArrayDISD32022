d2825110ace6dfc0999742b8d7d20e51ce97e2d5
Reece Wanvig <rwanvig@iie.ac.za>
2022-03-11 09:58:35 +0200

    added class and set the values inside the class

---
+++ UserData.php
@@ -0,0 +1,31 @@
+<?php
+    if (isset($UserData)==false)
+    {
+        $ud = new UserData();
+        $ud->setValues();
+        $ArrayTest=$ud->getArray();
+    }
+
+class UserData
+{
+    /**
+     * @var string[][]
+     */
+    private static $UserData2;
+
+    function  setValues()
+    {
+        self::$UserData2 = array(
+            array("1f9d9a9efc2f523b2f09629444632b5c","26269474bc6e14f4f9449c36056d92d3","ps. John"),
+            array("bbff37631b524c3aadd1b1b25e020e08","61409aa1fd47d4a5332de23cbf59a36f","Pastor John"),
+            array("3ee8a343f1c208a4836cff32b5237922","9477c9fc574c967285c7860c78d2c75a","Bishop Paul"),
+            array("ac946bf7babe63933f3eba52db544dec","3aa4584e547efba20238772d4af5c03d","Mr Pastor van Zyl"));
+
+    }
+    function getArray()
+    {
+        return self::$UserData2;
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
     session_start();
-
-    $UserData = array(
-            array("1f9d9a9efc2f523b2f09629444632b5c","26269474bc6e14f4f9449c36056d92d3","ps. John"),
-            array("bbff37631b524c3aadd1b1b25e020e08","61409aa1fd47d4a5332de23cbf59a36f","Pastor John"),
-            array("3ee8a343f1c208a4836cff32b5237922","9477c9fc574c967285c7860c78d2c75a","Bishop Paul"),
-            array("ac946bf7babe63933f3eba52db544dec","3aa4584e547efba20238772d4af5c03d","Mr Pastor van Zyl"));
-
+    include_once 'UserData.php';
 
     /*$UserData = array(
         array("john@gmail.com","Saint"),
@@ -74,7 +68,7 @@
     {
         $Logged = false;
 
-        $Array=$GLOBALS['UserData'];
+        $Array=$GLOBALS['ArrayTest'];
         for ($x =0;$x <count($Array);$x++)
         {
             if((strcmp($Array[$x][0],md5($EmailAddress))==0)&&(strcmp($Array[$x][1],md5($PasswordEntered))==0))

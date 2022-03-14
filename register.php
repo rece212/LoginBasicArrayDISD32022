f90f2e9fb20f749afc7cfaeece3d17292ab538cd
Reece Wanvig <rwanvig@iie.ac.za>
2022-03-14 09:55:22 +0200

    added array sessions

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
     session_start();
     print_r($_SESSION['Array']);
 
-    array_push($_SESSION['Array'],"399786750ec8ac94a1a3952bf858569a","098f6bcd4621d373cade4e832627b4f6","Test");
+    array_push($_SESSION['Array'],Array("399786750ec8ac94a1a3952bf858569a","098f6bcd4621d373cade4e832627b4f6","Test"));
     print_r($_SESSION['Array']);
 
 ?>
